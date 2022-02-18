0f00b078a559cdac281d8f8df2ca35918b37c3df
Majo Henao <mhenaod120@gmail.com>
2022-02-18 08:53:15 -0500

    Lab1-2

--- Lab1-1.c
+++ Lab1-1.c
@@ -1,1 +1,35 @@
-asd
+/**
+ * @file Lab1-1.c
+ * @author JUlio Echavarria (julioechavarria@gmail.com)
+ * @version 0.1
+ * @date 2022-02-18
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <stdio.h>
+
+
+void funcPrint(char *pstring);
+
+int main(void)
+{
+    funcPrint("Hola Mundo Cruel!!!");
+    return(0);
+}
+
+/**
+ * \brief funcPrint
+ * 
+ * funcPrint
+ * 
+ * \author XXX
+ * \param pstring This variable recieve
+ *                the direction of the string to be printed
+ * \date 18/02/2022 
+ */
+void funcPrint(char *pstring)
+{
+    printf("%s\n",pstring);
+}
